2efd58456d470b0a09940503b9fd0c4891f827ae
MPiiri <matthiaspiiri@gmail.com>
2025-03-17 21:04:45 +0200

    1.7

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 const Options = () => {
-  const [good, setGood] = React.useState(0);
-  const [neutral, setNeutral] = React.useState(0);
-  const [bad, setBad] = React.useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+  useEffect(() => {
+    setAll(good + neutral + bad);
+    if (all > 0) {
+      setAverage((good - bad) / all);
+      setPositive(good / all * 100);
+    }
+  }, [good, neutral, bad, average, positive, all]);
+
+  console.log("all", all);
+
   return (
     <div>
       <button onClick={() => setGood(good + 1)} value={good}>good</button>
@@ -13,6 +25,9 @@ const Options = () => {
         <p>good: {good}</p>
         <p>neutral {neutral}</p>
         <p>bad  {bad}</p>
+        <p>all  {all};</p>
+        <p>average  {average}</p>
+        <p>positive  {positive} %</p>
       </div>
     </div>
   );
